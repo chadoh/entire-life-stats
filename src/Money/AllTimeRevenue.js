2ca10356cad231fcaf1bdfa8527023d9a59bd983
Chad Ostrowski <hi@chadoh.com>
2016-11-29 21:45:06 -0500

    Improve graph styles

--- src/Money/AllTimeRevenue.js
+++ src/Money/AllTimeRevenue.js
@@ -35,21 +35,27 @@ export default ({data}) => {
         ${Math.round(Math.abs(total))} over the life of the business:
       </h2>
       <VictoryChart padding={{left: 15, top: 0, right: 25, bottom: 10}}>
-        <VictoryAxis tickFormat={dateToString} />
-        <VictoryAxis dependentAxis tickFormat={numberToMoney} />
         <VictoryLine
           data={accumulated}
           style={{
             data: {stroke: "tomato", strokeWidth: 4},
             labels: {fontSize: 12},
-            parent: {border: "1px solid #ccc"}
+            parent: {border: "1px solid #ccc"},
           }}
+          animate={{onEnter: {duration: 2000}}}
         />
+        <VictoryAxis tickFormat={dateToString} style={{ grid: {
+          stroke: "#ECEFF1",
+          strokeDasharray: "10, 5",
+          strokeLinecap: "round",
+          strokeLinejoin: "round"
+        }}}/>
+        <VictoryAxis dependentAxis tickFormat={numberToMoney} />
       </VictoryChart>
       <p>How to read this graph:</p>
       <ul>
-        <li>The starting downslope is the cost of hosting our API</li>
-        <li>The downturn in ~Feb 2016 is the cost of hosting our UI</li>
+        <li>The starting downslope is the cost of hosting our API (the behind-the-scenes “brain” of our app)</li>
+        <li>The downturn in ~Feb 2016 is the cost of hosting our UI (the stuff you see, click on, and interact with)</li>
         <li>The sharper downturn in July 2016 is the cost of a bigger database (more users = no more free-tier database)</li>
         <li>The up-ticks are when lovely people <a href="https://entire.life/emporium">paid for add-ons</a></li>
       </ul>
