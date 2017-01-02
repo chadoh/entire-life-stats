d6ad11826881ac5662cc5c41e24876b193f08c22
Chad Ostrowski <hi@chadoh.com>
2017-01-01 23:17:33 -0500

    Start setting up Users stats

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Money from '../Money';
+import Users from '../Users';
 
 export default class App extends Component {
   render() {
@@ -11,8 +12,17 @@ export default class App extends Component {
           stats.entire.life is charting the nitty-gritty business details of
           running <a href="https://entire.life">Entire.Life</a>
         </p>
-        <main className="App-main">
-          <Money />
+        <main className="App-main" role="main">
+          <div id="users">
+            <Users />
+          </div>
+          <div id="default">
+            <a href="#money">The Money</a>
+            <a href="#users">The Users</a>
+          </div>
+          <div id="money">
+            <Money />
+          </div>
         </main>
       </div>
     );
---
+++ src/Users/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const USERS_API = 'https://entire-life.herokuapp.com/stats/users';
+
+const toDate = str => {
+  const [year, month, day] = str.split("T")[0]
+    .split("-").slice(0, 3).map(Number);
+
+  return new Date(year, month - 1, day);
+};
+
+const toDatum = (multiplier = 1) => datum => ({
+  date: toDate(datum.created_at),
+  amount: multiplier * datum.amount / 100,
+});
+
+export default class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch(USERS_API)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({data: {
+          count: json.count,
+          users: json.users.map(toDatum()),
+        }});
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+    if (!data) return null;
+    return (
+      <div>
+        <h2>Here's how we got {data.count} people signed up for Entire.Life:</h2>
+      </div>
+    );
+  }
+}
